19e7baf209417ad00f42a9d26bb80f2b326b0bbd
SyntaxErrol <raina@kapsi.fi>
2024-01-08 12:14:33 +0200

    Space out the identifier

--- script.user.js
+++ script.user.js
@@ -3,15 +3,15 @@
 // @namespace   raina
 // @match       https://*.finna.fi/Holds/List
 // @grant       none
-// @version     1.0
+// @version     1.1.202418
 // @author      raina
-// @description 12/7/2023, 8:49:15 AM
 // ==/UserScript==
 let heading = document.querySelector(`h2`);
 let span = document.createElement("span");
 span.style.float = "right";
 span.style.lineHeight = 2;
 span.style.fontSize = "smaller";
+span.style.marginLeft = "1ex";
 span.textContent = localStorage.holdIdentifier ?? "";
 heading.append(span);
 fetch("/MyResearch/Profile")
